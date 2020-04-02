e5a4c1f5e3b148e214538523b80967ca485dafc1
Alan Willoughby <satyadvaya@gmail.com>
2020-04-02 12:56:40 -0700

    prep commit for lab 04

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const guess = document.getElementById('submitted-guess');
 // const result = document.getElementById('submitted-result');
 const wonOrLost = document.getElementById('won-or-lost');
 const guessResult = document.getElementById('guess-result');
-// const guessesToGo = document.getElementById('guesses-to-go');
+// const guessesLeft = document.getElementById('guesses-left');
 // const outcomeResponse = document.getElementById('outcome-response');
 // const resetButton = document.getElementById('reset');
 // const mainArea = document.getElementById('main-area');
 
-const generatedNumber = Math.floor(Math.random() * 20 + 1) ;
+let correctNumber = Math.floor(Math.random() * 20 + 1);
 
-const decision = (guess - generatedNumber);
+let decision = (guess - correctNumber);
 
 // function beginGame() {
 //     let correctAnswers = 0;
@@ -38,4 +38,6 @@ else if (decision === 1) {
     guessResult.textContent = 'Your guess was too high -- guess lower!';
 }
 
-// submitButton.addEventListener('click', beginGame);
+// submitButton.addEventListener('click', function(beginGame) {
+
+// })
